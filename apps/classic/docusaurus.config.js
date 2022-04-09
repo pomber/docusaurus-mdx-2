5dd4798305aff88358345a70ac3df85d5c6eabde
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-04-09 13:52:29 +0000

    Change index text

--- apps/classic/docusaurus.config.js
+++ apps/classic/docusaurus.config.js
@@ -7,14 +7,14 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "My Site",
-  tagline: "Dinosaurs are cool",
+  tagline: "MDX v2 Demo",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "pomber", // Usually your GitHub org/user name.
+  projectName: "docusaurus-mdx-2", // Usually your repo name.
 
   presets: [
     [
